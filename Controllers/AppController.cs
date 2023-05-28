5d08806b433e84f63604989524f9c6ce1a5755a0
Ibra-Alpha BA <ibbraba@gmail.com>
2023-05-28 21:01:16 +0200

    Init commit - Creating app and User API controllers

---
+++ Controllers/AppController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrowseClimate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppController : ControllerBase
+    {
+        [HttpGet]
+        [Route ("Index")]
+        public string Index()
+        {
+            return "hello world"; 
+        }
+
+        [HttpGet]
+        [Route("City")]
+        public string City(int id)
+        {
+
+            return "Temperature for a specific city + articles  + stories + stats? ";
+
+        }
+
+
+        [HttpGet]
+        [Route("Articles")]
+        public string Artcles()
+        {
+            return "All articles";
+        }
+
+        [HttpGet]
+        [Route("Article")]
+        public string Article (int id) {
+
+            return " single Articles";        
+        }
+        [HttpGet]
+        [Route("Stories")]
+        
+        public string Stories() {
+
+            return "all Stories";
+        }
+
+
+        [HttpGet]
+        [Route("Story")]
+        public string Story(int id) {
+
+            return "Single story";
+        }
+
+     
+
+
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrowseClimate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("MyPage")]
+        public string MyPage(int id)
+        {
+            return "Profile page";
+        }
+
+    }
+}
